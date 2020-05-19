f6d1d456c4ba81abc4423607b50ee61d727abafc
cwz <cwz@cwzdeMacBook-Pro.local>
2020-05-19 14:02:24 +0800

    Updates

---
+++ precache-manifest.dcb0db906d0c1fa99c488717f1901d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac43a4e08966427d00e4a8a7198b3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7411e761a24623a17878",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "152acf6489a65ab33014",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7411e761a24623a17878",
+    "url": "/static/js/2.bb3f707b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb3f707b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152acf6489a65ab33014",
+    "url": "/static/js/main.124c0dee.chunk.js"
+  },
+  {
+    "revision": "27439aac2f0ed14f67d8",
+    "url": "/static/js/runtime-main.4938474e.js"
+  },
+  {
+    "revision": "c25f50033da9ea713dbdaa7fdc4f4e64",
+    "url": "/static/media/loading.c25f5003.svg"
+  }
+]);
