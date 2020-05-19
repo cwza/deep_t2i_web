3c9528adda0c2010782c1ce7ed76c212742569d4
cwz <cwz@cwzdeMacBook-Pro.local>
2020-05-19 14:06:25 +0800

    Updates

---
+++ precache-manifest.24ee6150ed358ece0f78abd89733e749.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be2b355600ec5b8a7aa3e656f9eaea3",
+    "url": "/deep_t2i_server/index.html"
+  },
+  {
+    "revision": "7411e761a24623a17878",
+    "url": "/deep_t2i_server/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e0ee0e560aafb2bfdd8",
+    "url": "/deep_t2i_server/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7411e761a24623a17878",
+    "url": "/deep_t2i_server/static/js/2.bb3f707b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/deep_t2i_server/static/js/2.bb3f707b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0ee0e560aafb2bfdd8",
+    "url": "/deep_t2i_server/static/js/main.c0b46d8c.chunk.js"
+  },
+  {
+    "revision": "3eb35abc48d7c7aea505",
+    "url": "/deep_t2i_server/static/js/runtime-main.f5bb5220.js"
+  },
+  {
+    "revision": "c25f50033da9ea713dbdaa7fdc4f4e64",
+    "url": "/deep_t2i_server/static/media/loading.c25f5003.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcb0db906d0c1fa99c488717f1901d70.js"
+  "/deep_t2i_server/precache-manifest.24ee6150ed358ece0f78abd89733e749.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deep_t2i_server/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
